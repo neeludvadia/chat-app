219609289a984a1226ffc857051a995afe5ef1d3
neeludvadia <neel.udvadia@gmail.com>
2024-03-27 22:44:41 +0530

    registration page completed with database and api call

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,7 @@
-const { register } = require("../controllers/usersController");
+const  register  = require( "../controllers/usersController");
 
 const router = require("express").Router();
 
-router.post("/register",register)
+router.post("/register",register);
+
+module.exports = router;
