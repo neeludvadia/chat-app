d9565e7ef6d78755ae20b727b1caa71988c7e589
neeludvadia <neel.udvadia@gmail.com>
2024-03-28 16:03:52 +0530

    created login page with database api and started avatar select page with error

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,10 @@
-const  register  = require( "../controllers/usersController");
+const  { register}  = require( "../controllers/usersController");
+const {login} = require("../controllers/usersController")
+
 
 const router = require("express").Router();
 
 router.post("/register",register);
+router.post("/login",login);
 
 module.exports = router;
