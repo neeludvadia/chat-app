b50729b4065d490895460d05395eb3e100bfa0f2
neeludvadia <neel.udvadia@gmail.com>
2025-07-11 23:03:07 +0530

    added hosted frontend url in sockets

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server,{
   cors:{
-    origin:"`https://chat-ctzue4dnc-neeludvadias-projects.vercel.app`",
+    origin:"https://chat-app-six-psi-74.vercel.app/",
     credentials: true,
   }
 })
